66d8c215d4c396b5d78abba223d217bb1b074dc1
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-05-21 00:31:53 +0900

    初期化と終了実装

--- Assets/HKDebugger/Scripts/HKDebuggerSystem.cs
+++ Assets/HKDebugger/Scripts/HKDebuggerSystem.cs
@@ -8,8 +8,32 @@ namespace HKDebugger
     public class HKDebuggerSystem
     {
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        private static void Initialize()
+        private static void InitializeOnBeforeSceneLoad()
         {
+            Initialize();
+        }
+
+        private static GameObject rootObject;
+
+        public static void Initialize()
+        {
+            if (rootObject != null)
+            {
+                return;
+            }
+
+            rootObject = new GameObject("HKDebugger");
+            Object.DontDestroyOnLoad(rootObject);
+        }
+
+        public static void Finalize()
+        {
+            if (rootObject == null)
+            {
+                return;
+            }
+            
+            Object.Destroy(rootObject);
         }
     }
 }
